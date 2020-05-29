726ffc8ac829a3f3e8e6ac04ef3cd97cafb9ae1e
Mabeple <cymapu@gmail.com>
2020-05-29 21:57:41 +0800

    UITextFieldMPTests

--- Tests/UITextFieldMPTests.swift
+++ Tests/UITextFieldMPTests.swift
@@ -18,4 +18,90 @@ class UITextFieldMPTests: XCTestCase {
         textField.text = nil
         XCTAssert(textField.mp.isEmpty)
     }
+    
+    func testTrimmedText() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 30)
+        let textField = UITextField(frame: frame)
+        textField.text = "Hello \n    \n"
+        XCTAssertNotNil(textField.mp.trimmedText)
+        XCTAssertEqual(textField.mp.trimmedText!, "Hello")
+    }
+    
+    func testLeftViewTintColor() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 30)
+        let textField = UITextField(frame: frame)
+
+        let imageView = UIImageView()
+        imageView.tintColor = .red
+
+        textField.leftView = imageView
+        XCTAssertEqual(textField.mp.leftViewTintColor, .red)
+
+        textField.mp.leftViewTintColor = .blue
+        XCTAssertEqual(textField.mp.leftViewTintColor, .blue)
+
+        textField.leftView = nil
+        XCTAssertNil(textField.mp.leftViewTintColor)
+
+        textField.mp.leftViewTintColor = .yellow
+        XCTAssertNil(textField.mp.leftViewTintColor)
+    }
+    
+    func testRightViewTintColor() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 30)
+        let textField = UITextField(frame: frame)
+
+        let imageView = UIImageView()
+        imageView.tintColor = .red
+
+        textField.rightView = imageView
+        XCTAssertEqual(textField.mp.rightViewTintColor, .red)
+
+        textField.mp.rightViewTintColor = .blue
+        XCTAssertEqual(textField.mp.rightViewTintColor, .blue)
+
+        textField.rightView = nil
+        XCTAssertNil(textField.mp.rightViewTintColor)
+
+        textField.mp.rightViewTintColor = .yellow
+        XCTAssertNil(textField.mp.rightViewTintColor)
+    }
+
+    func testClear() {
+        let frame = CGRect(x: 0, y: 0, width: 100, height: 30)
+        let textField = UITextField(frame: frame)
+        textField.text = "Hello"
+        textField.mp.clear()
+        XCTAssertEqual(textField.text!, "")
+    }
+
+    func testSetPlaceHolderTextColor() {
+        let textField = UITextField()
+        textField.attributedPlaceholder = NSAttributedString(string: "Attributed Placeholder")
+        textField.mp.setPlaceHolderTextColor(.blue)
+        let color = textField.attributedPlaceholder?.attribute(.foregroundColor, at: 0, effectiveRange: nil) as? UIColor
+        XCTAssertEqual(color, .blue)
+
+        textField.placeholder = nil
+        textField.mp.setPlaceHolderTextColor(.yellow)
+        let emptyColor = textField.attributedPlaceholder?.attribute(.foregroundColor, at: 0, effectiveRange: nil) as? UIColor
+        XCTAssertNil(emptyColor)
+    }
+    
+    func testAddPaddingLeft() {
+        let textfield = UITextField()
+        textfield.frame = CGRect(x: 0, y: 0, width: 100, height: 30)
+        textfield.mp.addPaddingLeft(40)
+        XCTAssertEqual(textfield.leftView?.frame.width, 40)
+    }
+    
+//    func testAddPaddingImageIcon() {
+//        let textfield = UITextField()
+//        textfield.frame = CGRect(x: 0, y: 0, width: 100, height: 44)
+//
+//        let bundle = Bundle.init(for: UIImageExtensionsTests.self)
+//        let image = UIImage(named: "TestImage", in: bundle, compatibleWith: nil)!
+//        textfield.addPaddingLeftIcon(image, padding: 5)
+//        XCTAssertEqual(textfield.leftView?.frame.width, image.size.width + 5)
+//    }
 }
