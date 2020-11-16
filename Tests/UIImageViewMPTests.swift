c94599e718164669c0e47f767f6ce2efeb154711
Mabeple <cymapu@gmail.com>
2020-11-16 16:01:15 +0800

    Add UIImageView

---
+++ Tests/UIImageViewMPTests.swift
@@ -0,0 +1,35 @@
+//
+//  UIImageViewMPTests.swift
+//  Maple-iOSTests
+//
+//  Created by cy on 2020/11/16.
+//  Copyright © 2020 cy. All rights reserved.
+//
+
+import XCTest
+@testable import Maple
+
+#if canImport(UIKit) && !os(watchOS)
+import UIKit
+class UIImageViewMPTests: XCTestCase {
+
+    func testBlur() {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 100))
+        imageView.mp.blur(withStyle: .dark)
+
+        let blurView = imageView.subviews.first as? UIVisualEffectView
+        XCTAssertNotNil(blurView)
+        XCTAssertNotNil(blurView?.effect)
+        XCTAssertEqual(blurView?.frame, imageView.bounds)
+        XCTAssertEqual(blurView?.autoresizingMask, [.flexibleWidth, .flexibleHeight])
+        XCTAssert(imageView.clipsToBounds)
+    }
+
+    func testBlurred() {
+        let imageView = UIImageView()
+        let blurredImageView = imageView.mp.blurred(withStyle: .extraLight)
+        XCTAssertEqual(blurredImageView, imageView)
+    }
+
+}
+#endif
