6970d0e80c93593e72c0fe45e052dd63d9608bf1
Mabeple <cymapu@gmail.com>
2020-11-16 15:46:35 +0800

    Add UIAlertAction

---
+++ Sources/Maple/UIKit/UIAlertAction+Maple.swift
@@ -0,0 +1,22 @@
+//
+//  UIAlertAction+Maple.swift
+//  Maple
+//
+//  Created by cy on 2020/11/16.
+//  Copyright © 2020 cy. All rights reserved.
+//
+
+#if canImport(UIKit)
+import UIKit
+extension UIAlertAction: MabpleCompatible { }
+
+// MARK: - Properties
+public extension MabpleWrapper where Base == UIAlertAction {
+    
+    /// TitleTextColor
+    var titleTextColor: UIColor? {
+        get { base.value(forKey: "titleTextColor") as? UIColor }
+        set { base.setValue(newValue, forKey: "titleTextColor") }
+    }
+}
+#endif
