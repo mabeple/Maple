d58f09f0ccedd8acbd0182e3a1635aee82d50ab7
Mabeple <cymapu@gmail.com>
2020-11-16 15:15:41 +0800

    Add UIImage

---
+++ Sources/Maple/UIKit/UIImage+Maple.swift
@@ -0,0 +1,303 @@
+//
+//  UIImage+Maple.swift
+//  Maple-iOS
+//
+//  Created by cy on 2020/11/16.
+//  Copyright © 2020 cy. All rights reserved.
+//
+
+#if canImport(UIKit)
+import UIKit
+extension UIImage: MabpleCompatible { }
+
+// MARK: - Properties
+public extension MabpleWrapper where Base == UIImage {
+    
+    /// Size in bytes of UIImage
+    var bytesSize: Int {
+        return base.jpegData(compressionQuality: 1)?.count ?? 0
+    }
+
+    /// Size in kilo bytes of UIImage
+    var kilobytesSize: Int {
+        return (base.jpegData(compressionQuality: 1)?.count ?? 0) / 1024
+    }
+
+    /// UIImage with .alwaysOriginal rendering mode.
+    var original: UIImage {
+        return base.withRenderingMode(.alwaysOriginal)
+    }
+
+    /// UIImage with .alwaysTemplate rendering mode.
+    var template: UIImage {
+        return base.withRenderingMode(.alwaysTemplate)
+    }
+}
+
+// MARK: - Methods
+public extension MabpleWrapper where Base == UIImage {
+    
+    /// Compressed UIImage from original UIImage.
+    ///
+    /// - Parameter quality: The quality of the resulting JPEG image, expressed as a value from 0.0 to 1.0. The value 0.0 represents the maximum compression (or lowest quality) while the value 1.0 represents the least compression (or best quality), (default is 0.5).
+    /// - Returns: optional UIImage (if applicable).
+    func compressed(quality: CGFloat = 0.5) -> UIImage? {
+        guard let data = base.jpegData(compressionQuality: quality) else { return nil }
+        return UIImage(data: data)
+    }
+    
+    /// Compressed UIImage data from original UIImage.
+    ///
+    /// - Parameter quality: The quality of the resulting JPEG image, expressed as a value from 0.0 to 1.0. The value 0.0 represents the maximum compression (or lowest quality) while the value 1.0 represents the least compression (or best quality), (default is 0.5).
+    /// - Returns: optional Data (if applicable).
+    func compressedData(quality: CGFloat = 0.5) -> Data? {
+        return base.jpegData(compressionQuality: quality)
+    }
+    
+    /// UIImage Cropped to CGRect.
+    ///
+    /// - Parameter rect: CGRect to crop UIImage to.
+    /// - Returns: cropped UIImage
+    func cropped(to rect: CGRect) -> UIImage {
+        guard rect.size.width <= base.size.width, rect.size.height <= base.size.height else { return base }
+        let scaledRect = rect.applying(CGAffineTransform(scaleX: base.scale, y: base.scale))
+        guard let image = base.cgImage?.cropping(to: scaledRect) else { return base }
+        return UIImage(cgImage: image, scale: base.scale, orientation: base.imageOrientation)
+    }
+    
+    /// UIImage scaled to height with respect to aspect ratio.
+    ///
+    /// - Parameters:
+    ///   - toHeight: new height.
+    ///   - opaque: flag indicating whether the bitmap is opaque.
+    /// - Returns: optional scaled UIImage (if applicable).
+    func scaled(toHeight: CGFloat, opaque: Bool = false) -> UIImage? {
+        let scale = toHeight / base.size.height
+        let newWidth = base.size.width * scale
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: newWidth, height: toHeight), opaque, base.scale)
+        base.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: toHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    /// UIImage scaled to width with respect to aspect ratio.
+    ///
+    /// - Parameters:
+    ///   - toWidth: new width.
+    ///   - opaque: flag indicating whether the bitmap is opaque.
+    /// - Returns: optional scaled UIImage (if applicable).
+    func scaled(toWidth: CGFloat, opaque: Bool = false) -> UIImage? {
+        let scale = toWidth / base.size.width
+        let newHeight = base.size.height * scale
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: toWidth, height: newHeight), opaque, base.scale)
+        base.draw(in: CGRect(x: 0, y: 0, width: toWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    /// Creates a copy of the receiver rotated by the given angle (in radians).
+    ///
+    ///     // Rotate the image by 180°
+    ///     image.rotated(by: .pi)
+    ///
+    /// - Parameter radians: The angle, in radians, by which to rotate the image.
+    /// - Returns: A new image rotated by the given angle.
+    func rotated(by radians: CGFloat) -> UIImage? {
+        let destRect = CGRect(origin: .zero, size: base.size)
+            .applying(CGAffineTransform(rotationAngle: radians))
+        let roundedDestRect = CGRect(x: destRect.origin.x.rounded(),
+                                     y: destRect.origin.y.rounded(),
+                                     width: destRect.width.rounded(),
+                                     height: destRect.height.rounded())
+
+        UIGraphicsBeginImageContextWithOptions(roundedDestRect.size, false, base.scale)
+        guard let contextRef = UIGraphicsGetCurrentContext() else { return nil }
+
+        contextRef.translateBy(x: roundedDestRect.width / 2, y: roundedDestRect.height / 2)
+        contextRef.rotate(by: radians)
+
+        base.draw(in: CGRect(origin: CGPoint(x: -base.size.width / 2,
+                                             y: -base.size.height / 2),
+                             size: base.size))
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    /// UIImage filled with color
+    ///
+    /// - Parameter color: color to fill image with.
+    /// - Returns: UIImage filled with given color.
+    func filled(withColor color: UIColor) -> UIImage {
+        #if !os(watchOS)
+        if #available(tvOS 10.0, *) {
+            let format = UIGraphicsImageRendererFormat()
+            format.scale = base.scale
+            let renderer = UIGraphicsImageRenderer(size: base.size, format: format)
+            return renderer.image { context in
+                color.setFill()
+                context.fill(CGRect(origin: .zero, size: base.size))
+            }
+        }
+        #endif
+
+        UIGraphicsBeginImageContextWithOptions(base.size, false, base.scale)
+        color.setFill()
+        guard let context = UIGraphicsGetCurrentContext() else { return base }
+
+        context.translateBy(x: 0, y: base.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.setBlendMode(CGBlendMode.normal)
+
+        let rect = CGRect(x: 0, y: 0, width: base.size.width, height: base.size.height)
+        guard let mask = base.cgImage else { return base }
+        context.clip(to: rect, mask: mask)
+        context.fill(rect)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    /// UIImage tinted with color
+    ///
+    /// - Parameters:
+    ///   - color: color to tint image with.
+    ///   - blendMode: how to blend the tint
+    /// - Returns: UIImage tinted with given color.
+    func tint(_ color: UIColor, blendMode: CGBlendMode, alpha: CGFloat = 1.0) -> UIImage {
+        let drawRect = CGRect(origin: .zero, size: base.size)
+
+        #if !os(watchOS)
+        if #available(tvOS 10.0, *) {
+            let format = UIGraphicsImageRendererFormat()
+            format.scale = base.scale
+            return UIGraphicsImageRenderer(size: base.size, format: format).image { context in
+                color.setFill()
+                context.fill(drawRect)
+                base.draw(in: drawRect, blendMode: blendMode, alpha: alpha)
+            }
+        }
+        #endif
+
+        UIGraphicsBeginImageContextWithOptions(base.size, false, base.scale)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        let context = UIGraphicsGetCurrentContext()
+        color.setFill()
+        context?.fill(drawRect)
+        base.draw(in: drawRect, blendMode: blendMode, alpha: alpha)
+        return UIGraphicsGetImageFromCurrentImageContext()!
+    }
+
+    /// UImage with background color
+    ///
+    /// - Parameters:
+    ///   - backgroundColor: Color to use as background color
+    /// - Returns: UIImage with a background color that is visible where alpha < 1
+    func withBackgroundColor(_ backgroundColor: UIColor) -> UIImage {
+        #if !os(watchOS)
+        if #available(tvOS 10.0, *) {
+            let format = UIGraphicsImageRendererFormat()
+            format.scale = base.scale
+            return UIGraphicsImageRenderer(size: base.size, format: format).image { context in
+                backgroundColor.setFill()
+                context.fill(context.format.bounds)
+                base.draw(at: .zero)
+            }
+        }
+        #endif
+
+        UIGraphicsBeginImageContextWithOptions(base.size, false, base.scale)
+        defer { UIGraphicsEndImageContext() }
+
+        backgroundColor.setFill()
+        UIRectFill(CGRect(origin: .zero, size: base.size))
+        base.draw(at: .zero)
+        return UIGraphicsGetImageFromCurrentImageContext()!
+    }
+
+    /// UIImage with rounded corners
+    ///
+    /// - Parameters:
+    ///   - radius: corner radius (optional), resulting image will be round if unspecified
+    /// - Returns: UIImage with all corners rounded
+    func withRoundedCorners(radius: CGFloat? = nil) -> UIImage? {
+        let maxRadius = min(base.size.width, base.size.height) / 2
+        let cornerRadius: CGFloat
+        if let radius = radius, radius > 0, radius <= maxRadius {
+            cornerRadius = radius
+        } else {
+            cornerRadius = maxRadius
+        }
+
+        UIGraphicsBeginImageContextWithOptions(base.size, false, base.scale)
+
+        let rect = CGRect(origin: .zero, size: base.size)
+        UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius).addClip()
+        base.draw(in: rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+
+    /// Base 64 encoded PNG data of the image.
+    ///
+    /// - returns: Base 64 encoded PNG data of the image as a String.
+    func pngBase64String() -> String? {
+        base.pngData()?.base64EncodedString()
+    }
+
+    /// Base 64 encoded JPEG data of the image.
+    ///
+    /// - parameter compressionQuality: The quality of the resulting JPEG image, expressed as a value from 0.0 to 1.0. The value 0.0 represents the maximum compression (or lowest quality) while the value 1.0 represents the least compression (or best quality).
+    /// - returns: Base 64 encoded JPEG data of the image as a String.
+    func jpegBase64String(compressionQuality: CGFloat) -> String? {
+        base.jpegData(compressionQuality: compressionQuality)?.base64EncodedString()
+    }
+}
+
+// MARK: - Initializers
+public extension UIImage {
+    
+    /// Create UIImage from color and size.
+    ///
+    /// - Parameters:
+    ///   - color: image fill color.
+    ///   - size: image size.
+    convenience init(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        UIGraphicsBeginImageContextWithOptions(size, false, 1)
+
+        defer {
+            UIGraphicsEndImageContext()
+        }
+
+        color.setFill()
+        UIRectFill(CGRect(origin: .zero, size: size))
+
+        guard let aCgImage = UIGraphicsGetImageFromCurrentImageContext()?.cgImage else {
+            self.init()
+            return
+        }
+
+        self.init(cgImage: aCgImage)
+    }
+    
+    /// Create a new image from a base 64 string.
+    ///
+    ///
+    /// - Parameters:
+    ///   - base64String: a base-64 `String`, representing the image
+    ///   - scale: The scale factor to assume when interpreting the image data created from the base-64 string. Applying a scale factor of 1.0 results in an image whose size matches the pixel-based dimensions of the image. Applying a different scale factor changes the size of the image as reported by the `size` property.
+    convenience init?(base64String: String, scale: CGFloat = 1.0) {
+        guard let data = Data(base64Encoded: base64String) else { return nil }
+        self.init(data: data, scale: scale)
+    }
+}
+
+#endif
