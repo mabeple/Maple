090e9697ff33c296cedf587ddd594cfbdbfef683
Mabeple <cymapu@gmail.com>
2020-11-16 15:46:44 +0800

    修改格式

--- Sources/Maple/Shared/ColorExtensions.swift
+++ Sources/Maple/Shared/ColorExtensions.swift
@@ -248,6 +248,21 @@ public extension MPCrossPlatformColor {
     }
     #endif
     
+    #if canImport(UIKit) && !os(watchOS)
+    /// Create a UIColor with different colors for light and dark mode.
+    ///
+    /// - Parameters:
+    ///     - light: Color to use in light/unspecified mode.
+    ///     - dark: Color to use in dark mode.
+    convenience init(light: UIColor, dark: UIColor) {
+        if #available(iOS 13.0, tvOS 13.0, *) {
+            self.init(dynamicProvider: { $0.userInterfaceStyle == .dark ? dark : light })
+        } else {
+            self.init(cgColor: light.cgColor)
+        }
+    }
+    #endif
+    
     /// Random color.
     static var random: MPCrossPlatformColor {
         let red = Int.random(in: 0...255)
