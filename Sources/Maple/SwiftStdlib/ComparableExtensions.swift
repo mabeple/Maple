56d28a7020d02d04611501bd2d23be528cbbdc9a
mabeple <cymapu@gmail.com>
2025-12-30 11:45:04 +0800

    …

--- Sources/Maple/SwiftStdlib/ComparableExtensions.swift
+++ Sources/Maple/SwiftStdlib/ComparableExtensions.swift
@@ -8,41 +8,34 @@
 
 import Foundation
 
-// MARK: - MapleCompatible
-
-extension Comparable {
-    public var mp: MapleWrapper<Self> {
-        get { MapleWrapper(self) }
-        set { }
-    }
-}
 // MARK: - Methods
 
-public extension MapleWrapper where Base: Comparable {
+public extension Comparable {
+
     /// Returns true if value is in the provided range.
     ///
-    ///    1.mp.isBetween(5...7) // false
-    ///    7.mp.isBetween(6...12) // true
-    ///    date.mp.isBetween(date1...date2)
-    ///    "c".mp.isBetween(a...d) // true
-    ///    0.32.mp.isBetween(0.31...0.33) // true
+    ///     1.isBetween(5...7) // false
+    ///     7.isBetween(6...12) // true
+    ///     date.isBetween(date1...date2)
+    ///     "c".isBetween("a"..."d") // true
+    ///     0.32.isBetween(0.31...0.33) // true
     ///
     /// - Parameter range: Closed range against which the value is checked to be included.
     /// - Returns: `true` if the value is included in the range, `false` otherwise.
-    func isBetween(_ range: ClosedRange<Base>) -> Bool {
-        return range ~= base
+    func isBetween(_ range: ClosedRange<Self>) -> Bool {
+        range.contains(self)
     }
 
     /// Returns value limited within the provided range.
     ///
-    ///     1.mp.clamped(to: 3...8) // 3
-    ///     4.mp.clamped(to: 3...7) // 4
-    ///     "c".mp.clamped(to: "e"..."g") // "e"
-    ///     0.32.mp.clamped(to: 0.1...0.29) // 0.29
+    ///     1.clamped(to: 3...8) // 3
+    ///     4.clamped(to: 3...7) // 4
+    ///     "c".clamped(to: "e"..."g") // "e"
+    ///     0.32.clamped(to: 0.1...0.29) // 0.29
     ///
     /// - Parameter range: Closed range that limits the value.
     /// - Returns: A value limited to the range, i.e. between `range.lowerBound` and `range.upperBound`.
-    func clamped(to range: ClosedRange<Base>) -> Base {
-        return max(range.lowerBound, min(base, range.upperBound))
+    func clamped(to range: ClosedRange<Self>) -> Self {
+        min(max(self, range.lowerBound), range.upperBound)
     }
 }
